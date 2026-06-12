2c19a16e72d744db8026751853954b372293d2fa
Luis Chang <luis.chang@outlook.com>
2026-06-11 20:44:25 -0500

    Cambiar a SQL Server y añadir soporte para CORS
    Se ha actualizado la configuración de la base de datos en `Program.cs`
    para usar SQL Server como proveedor predeterminado. La configuración
    de PostgreSQL ha sido comentada para referencia futura.
    
    Se ha añadido una política de CORS llamada "AllowAll" en `Program.cs`
    y se ha habilitado en el pipeline de la aplicación con
    `app.UseCors("AllowAll")`, permitiendo solicitudes desde cualquier
    origen, método y encabezado.
    
    En `appsettings.json`, se ha actualizado la cadena de conexión
    `DevConnection` para apuntar a un servidor SQL Server local. La
    configuración previa para PostgreSQL ha sido comentada.

--- UESAN.SHOPPING.API/Program.cs
+++ UESAN.SHOPPING.API/Program.cs
@@ -11,12 +11,11 @@ var builder = WebApplication.CreateBuilder(args);
 var _config = builder.Configuration;
 var cnx = _config.GetConnectionString("DevConnection");
 //SQL Server
-
-//builder.Services.AddDbContext<StoreDbContext>(options =>
-//  options.UseSqlServer(cnx));
-// PostreSQL
 builder.Services.AddDbContext<StoreDbContext>(options =>
-  options.UseNpgsql(cnx));
+  options.UseSqlServer(cnx));
+// PostreSQL
+//builder.Services.AddDbContext<StoreDbContext>(options =>
+  //options.UseNpgsql(cnx));
 
 builder.Services.AddTransient<ICategoryRepository, CategoryRepository>();
 builder.Services.AddTransient<ICategoryService, CategoryService>();
@@ -29,6 +28,20 @@ builder.Services.AddSharedInfrastructure(_config);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+// Add CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        //WithOrigins("http://localhost:3000") // Reemplaza con el origen de tu frontend
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -40,6 +53,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseAuthorization();
+app.UseCors("AllowAll");
 
 app.MapControllers();
 
